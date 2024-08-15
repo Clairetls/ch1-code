2fcb18df698de960ce66a7e164ce539b6f42ed12
Clairetls <l.s.tsui@rug.nl>
2024-08-15 16:17:31 +0200

    new stuff 15/08/2024

--- asymmetry bivar ver 3.R
+++ asymmetry bivar ver 3.R
@@ -8,9 +8,7 @@ library(lme4)
 library(brms)
 library(glmm)
 library(lmerTest)
-library(APCtools)
-library(modelsummary)
-library(flextable)
+library(MCMCglmm)
 
 # modelsummary(body_28mod)
 ###################
@@ -19,8 +17,6 @@ provisioning<-read.csv("provisioning_28_5.csv",stringsAsFactors = F, sep = ';',d
 
 haematocrit<-read.csv("haematocrit_28_5.csv",stringsAsFactors = F)
 telomeres<-read.csv("telomere_28_5.csv", stringsAsFactors = F)
-
-
 
 
 
@@ -71,7 +67,7 @@ bodyprov$prate_z<-scale(bodyprov$prate)
 bodyprov$RightTarsus_z<-scale(bodyprov$newtars)
 
 
-postdeclinebp<-bodyprov%>%filter(age_year>5)
+postdeclinebp<-bodyprov%>%filter(age_year>4)
 
 bodyprov$SexEstimate<-as.factor(bodyprov$SexEstimate)
 bodyprov$newstat<-as.factor(bodyprov$newstat)
@@ -93,7 +89,7 @@ solve(crossprod(bp$family$data$R)) ## estimated cov matrix
 plot(trial, pages=1)
 plot(bp, pages = 1)
 
-solve(crossprod(bp$family$data$R)) ## estimated cov matrix
+# solve(crossprod(bp$family$data$R)) ## estimated cov matrix
 
 #0.01575861
 #           [,1]       [,2]
@@ -113,12 +109,12 @@ thing<-postdeclinebp%>%
   summarize(count=length(BirdID))
 
 bmfit<-bf(BodyMass_z~age_year+I(age_year)^2+newlifespan+RightTarsus_z+SexEstimate+avg_invert.y+
-            newstat.y+(1|birthyear)+(1|BirdID))
+            newstat.y+(1|birthyear)+(1|q|BirdID))
 prfit<-bf(prate_z~age_year+I(age_year)^2+newlifespan+SexEstimate+BroodSize+nr_helpers+
-            avg_invert.x+newstat.x+(1|birthyear)+(1|BirdID))
+            avg_invert.x+newstat.x+(1|birthyear)+(1|q|BirdID))
 fitprior <- get_prior(bmfit+prfit, data = postdeclinebp)
 fitprior
-fit<-brm(bmfit+prfit, data = postdeclinebp, chains = 4,iter=10000,
+fit<-brm(bmfit+prfit+set_rescor(TRUE), data = postdeclinebp, chains = 4,iter=10000,
          warmup = 1000, thin = 5, prior = fitprior)
 fit
 plot(fit)
@@ -131,32 +127,21 @@ postdeclinebp$SexEstimate<-as.factor(postdeclinebp$SexEstimate)
 postdeclinebp$BirdID<-as.factor(postdeclinebp$BirdID)
 postdeclinebp$birthyear<-as.factor(postdeclinebp$birthyear)
 
-postbp<-gam(list(BodyMass_z~s(age_year, k=-1)+newlifespan+SexEstimate+newstat+
-               avg_invert+RightTarsus_z+s(birthyear, bs='re')+s(age_year, BirdID, bs="re"),
-             prate_z~ s(age_year, k=-1)+newlifespan+SexEstimate+newstat+BroodSize+
-               nr_helpers+avg_invert+s(birthyear, bs='re')+s(age_year,BirdID, bs="re")),
+postbp<-gam(list(BodyMass_z~s(age_year, k=-1)+newlifespan+SexEstimate+newstat.x+
+               avg_invert.x+RightTarsus_z+s(birthyear, bs='re')+s(BirdID, bs="re"),
+             prate_z~ s(age_year, k=-1)+newlifespan+SexEstimate+newstat.y+BroodSize+
+               nr_helpers+avg_invert.y+s(birthyear, bs='re')+s(BirdID, bs="re")),
         family=mvn(d=2),method="REML",data=postdeclinebp)
 
 summary(postbp)
 plot(postbp, pages=1)
 solve(crossprod(postbp$family$data$R)) ## estimated cov matrix
 
+        # [,1]         [,2]
+# [1,]  0.042942958 -0.008214547
+# [2,] -0.008214547  0.753117459
 
-
-######################################################################
-#bodymass and survival
-
-survival<-survival[,c('BirdID',"age","birthyear",'survival',
-                      'SexEstimate',"statbefore",'avg_bug','occasionyear')]
-names(survival)[names(survival)=="age"]<-'age_year'
-bsdata<-left_join(survival, bmdata, by = c("age_year", "BirdID", 'SexEstimate',
-                                           'birthyear'))
-
-
-
-#er whut do i do..
-#okay mich said it was useless so no
-
+as_flextable(postbp)
 
 
 
@@ -177,12 +162,31 @@ telodata<-telomeres[,c("BirdID",'age_year',"SexEstimate",'birthyear','avg_invert
 telofat<-left_join(fatdata, telodata, by=c('BirdID', "age_year", 'SexEstimate',
                                            'CatchID','newlifespan','birthyear','newstat'))
 
+telofat<-filter(telofat, !is.na(telofat$RTL))
+telofat<-na.omit(telofat)
+
 telofat$RTL_z<-scale(telofat$RTL)
 
-library(MCMCglmm)
 telofat$SexEstimate<-as.factor(telofat$SexEstimate)
 
-write.csv(telofat, "telofat.csv")
+sd(telofat$RTL_z)
+
+prior<-list(R=list(V=diag(2)/2,nu=0.002),
+            G=list(G1=list(V=diag(2)/2,nu=0.002),
+                   G2=list(V=diag(2)/2, nu=0.002)))
+
+
+# prior = prior1,
+tfmcmc<-MCMCglmm(cbind(FatScore, RTL_z)~age_year+newlifespan+SexEstimate+
+               newstat+avg_invert.x,
+             random = ~us(1+age_year):BirdID + us(trait):birthyear, rcov=~us(trait):units,
+             family = c('poisson', 'gaussian'), prior=prior,
+             data=telofat, nitt = 1000000, burnin=5000, thin=100)
+
+plot(tfmcmc)
+autocorr(tfmcmc$VCV)
+autocorr(tfmcmc$Sol)
+summary(tfmcmc)
 
 #attempt GLMM
 #telo and fat score is linear
@@ -190,27 +194,28 @@ write.csv(telofat, "telofat.csv")
 
 View(telofat)
 
-sum(is.na(telofat$FatScore))
+
+tfmod<-readRDS("tf_model.rds")
+plot(tfmod)
+autocorr(tfmod$VCV)
+autocorr(tfmod$Sol)
+summary(tfmod)
 
 
 
+# #example model-- needs tweaking + priors and other variables
+# #prior is wrong
+# prior1<-list(G = list(G1 = list(v=diag(2), nu=0.002),
+#                      G2 = list(V=diag(2), nu=0.002)),
+#             R= list(R1=list(v=diag(2), nu=0.002),
+#                     R2=list(v=diag(2), nu=0.002)))
+#
+# tf<-MCMCglmm(cbind(FatScore, RTL_z)~age_year+newlifespan+SexEstimate+newstat+avg_invert,
+#          random = ~us(trait):BirdID + us(trait):birthyear, rcov=~us(trait):units,
+#          family = c('poisson', 'gaussian'), prior = prior1,
+#          data=telofat, nitt = 10000000, burnin=10000, thin=100)
 
-
-#example model-- needs tweaking + priors and other variables
-#prior is wrong
-prior1<-list(G = list(G1 = list(v=diag(2), nu=0.002),
-                     G2 = list(V=diag(2), nu=0.002)),
-            R= list(R1=list(v=diag(2), nu=0.002),
-                    R2=list(v=diag(2), nu=0.002)))
-
-tf<-MCMCglmm(cbind(FatScore, RTL_z)~age_year+newlifespan+SexEstimate+newstat+avg_invert,
-         random = ~us(trait):BirdID + us(trait):birthyear, rcov=~us(trait):units,
-         family = c('poisson', 'gaussian'), prior = prior1,
-         data=telofat, nitt = 10000000, burnin=10000, thin=100)
-
-
-
-summary(tf)
+# summary(tf)
 
 #covar is -0.08
 
@@ -221,13 +226,13 @@ summary(tf)
 #when i plotted the model the slopes were wildly different than original models
 #I then ran fat score separately in a gam with poisson and gaussian, the gaussian had a very different curve than poisson
 
-telofat2<-filter(telofat, !is.na(telofat$RTL_z))
-
-#simple test model
-test2<-gam(list(FatScore~s(age_year)+newlifespan,
-                RTL_z~s(age_year)+newlifespan), family = mvn(d=2), data=telofat2, method='REML')
-plot(test2, pages = 1)
-solve(crossprod(test2$family$data$R))
+# telofat2<-filter(telofat, !is.na(telofat$RTL_z))
+#
+# #simple test model
+# test2<-gam(list(FatScore~s(age_year)+newlifespan,
+#                 RTL_z~s(age_year)+newlifespan), family = mvn(d=2), data=telofat2, method='REML')
+# plot(test2, pages = 1)
+# solve(crossprod(test2$family$data$R))
 
 #covar is -0.02
 
@@ -245,6 +250,9 @@ telo_simple<-gam(RTL_z~s(age_year)+newlifespan, family=gaussian, method="REML", 
 plot(telo_simple)
 
 plot(telodata$age, telodata$RTL_z)
+
+#yes it does
+
 
 
 ##############################################
@@ -278,75 +286,75 @@ tb<-gam(list(RTL_z~s(age_year)+newlifespan+avg_invert.x+newstat.x+
 
 solve(crossprod(tb$family$data$R))
 
+
+
 #0.0178214
 
 ############################
 haemfat<- left_join(haemf, fatdata, by=c('BirdID',"age_year", 'newlifespan',
-                                         'newstat','birthyear',"CatchID"))
+                                         'newstat','birthyear', "CatchID"))
+
+haemfat<-filter(haemfat, !is.na(haemfat$FatScore))
+haemfat$wbc_z<-scale(haemfat$wbc)
+haemfat$FatScorelog<-log(haemfat$FatScore)
+
+haemfit<-bf(wbc_z~age_year+I(age_year)^2+newlifespan+avg_invert.x+
+              newstat+(age_year|q|BirdID))
+fatfit<-bf(FatScorelog~age_year+I(age_year)^2+newlifespan+avg_invert.y+
+             newstat+(age_year|q|BirdID))
+
+hffit<-bf(mvbind(wbc_z,FatScorelog)~age_year+I(age_year)^2+newlifespan+
+            avg_invert.x+newstat+(age_year|q|BirdID))
+
+hfprior<-get_prior(haemfit+fatfit, data=haemfat)
+
+trial<-brm(haemfit+fatfit+set_rescor(TRUE), data = haemfat,family=list(gaussian(),gaussian()),
+           chains = 4,iter=10000, warmup = 100, thin = 10)
+
+summary(trial)
+
+hfprior<-c(set_prior('lkj(2)', class="b", resp='y1'),
+           set_prior('student_t(3,0,2.5)', class = 'sigma', resp='y1'),
+           set_prior('student_t(3, -0.1, 2.5)', class = 'intercept', resp='y1'),
+
+)
+mean(haemfat$FatScore)
+
+hffit<-brm(haemfit+fatfit+set_rescor(TRUE), data = haemfat,
+           chains = 4,iter=10000, warmup = 1000, thin = 5, prior = hfprior)
+
+#######################################
+#with MCMCglmm
+
+mcprior<-list(R=list(V=diag(2)/0.5,nu=2),
+            G=list(G1=list(V=diag(2)/0.5, nu=3)))
+
+mcprior2<-list(R=list(V=diag(2)/2,nu=2),
+               G=list(G1=list(V=diag(2)/2, nu=3)))
+
+#G is for random effects, R is for residual
 
 
-sum(is.na(haemfat$FatScore))
+hfmcmc<-MCMCglmm(cbind(wbc_z,FatScore)~age_year+newlifespan+
+                   avg_invert.x+newstat,
+                 random = ~us(1+age_year):BirdID, rcov = ~us(trait):units,
+         prior=mcprior,family=c('gaussian', 'poisson'),data=haemfat,
+         nitt = 100000, burnin=5000, thin=50)
+
+saveRDS(hfmcmc, 'haemfatmc.rds')
+
+plot(hfmcmc)
+summary(hfmcmc)
+autocorr(hfmcmc$VCV)
+autocorr(hfmcmc$Sol)
 
 
-#the above are examples of doable models
-
-#this model has no solution so far
-malaria<-read.csv("malaria_28_5.csv", sep = ';', dec = ',')
-
-mdata<-malaria[,c('BirdID', "age","newlifespan",'newstat',"birthyear","CatchID",
-                "avg_invert","TestResult",'FieldPeriodID')]
-haem_m<-filter(haematocrit, haematocrit$SexEstimate==1)
-haem_m<-haem_m[,c('BirdID', "age","newlifespan",'newstat',"birthyear","CatchID",
-                "avg_invert","Observer","wbc", "CatchTime",'FieldPeriodID')]
+cov_age_year <- hfmcmc$VCV[,c(5:8)]
+colnames(cov_age_year)<-c('wbc','fatwbc','wbcfat','fat')
+View(cov_age_year)
 
 
-malhaem<-left_join(haem_m, mdata, by.x=c('BirdID', 'age','newlifespan','birthyear', "CatchID",'FieldPeriodID'))
-
-
-
-test1234<-malhaem%>%
-  group_by(age,BirdID, FieldPeriodID)%>%
-  mutate(malcount=length(TestResult))%>%
-  mutate(haemcount=length(wbc))%>%
-  mutate(newwbc=ifelse(haemcount>1, mean(wbc, na.rm=T),wbc))
-
-
-test1234<-test1234[,-c(6,8,10)]
-test1234<-unique(test1234)
-
-test1234$wbc_z<-scale(test1234$newwbc)
-
-test1234<-test1234%>%filter(!is.na(TestResult))
-#not linear nor quadratic
-#different families
-
-#possible options:
-
-
-mh1<-gam(list(TestResult~s(age)+newlifespan+s(BirdID, bs="re"),
-              wbc_z~s(age)+newlifespan+s(BirdID, bs="re")),
-         family=mvn(d=2), method="REML", data=test1234)
-
-plot(mh1, pages = 1)
-solve(crossprod(mh1$family$data$R))
-0.04458811
-
-#possibly fine, only thing is malaria test result is binary 1,0
-
-
-test1234<-as.data.frame(test1234)
-mh2<-MCMCglmm(cbind(TestResult, wbc_z)~age+I(age^2)+newlifespan,
-             random = ~us(trait):BirdID, rcov=~us(trait):units,
-             family = c('categorical', 'gaussian'),data=test1234, nitt = 10000, burnin=1000, thin=50)
-
-
-summary(mh2)
--0.06443
-
-#different slope if gaussian vs poisson
-#mvn2 probably doesnt satisfy the assumptions, so fat has a positive slope now
-#mcmcglmm- must use square term
-#or fine if linear?
-#maybe i can use a mix of both..
-
-
+library(modelsummary)
+library(broom.mixed)
+thing<-tidy(tfmod)
+tidyMCMC(thing)
